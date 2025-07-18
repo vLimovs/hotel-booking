b3990f4bd43f12b8ec1c4b4e5dc934e69e1e973f
vLimovs <limonytchannel@gmail.com>
2025-07-18 23:52:54 +0500

    Added: Admin Panel, Modal window to suggest adding a new hotel.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
+import AddRoom from './pages/hotelOwner/AddRoom';
+import ListRoom from './pages/hotelOwner/ListRoom';
 
 const App = () => {
 
@@ -14,13 +19,19 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && < Navbar />}
+      {false && <HotelReg />}
       <div className='min-h-[70vh]'>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/rooms' element={<AllRooms />}/>
-        <Route path='/rooms/:id' element={<RoomDetails />}/>
-        <Route path='/my-bookings' element={<MyBookings />}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
+          <Route path='/my-bookings' element={<MyBookings />} />
+          <Route path='/owner' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='add-room' element={<AddRoom />} />
+            <Route path='list-room' element={<ListRoom />} />
+          </Route>
+        </Routes>
       </div>
       <Footer />
     </div>
