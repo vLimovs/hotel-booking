ecfcae11ad660775676800cf629aef562638050b
vLimovs <limonytchannel@gmail.com>
2025-07-27 21:37:57 +0500

    fix1

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { registerHotel } from "../configs/controllers/hotelController";
+import { registerHotel } from "../configs/controllers/hotelController.js";
 
 const hotelRouter = express.Router()
 
